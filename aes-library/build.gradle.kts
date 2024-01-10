939ef6468aea48f6a367f51ff4e56b4a1a92850d
Asadullah Ilyas <asadullah.ilyas@packagex.io>
2024-01-10 18:26:50 +0500

    Java version updated

--- aes-library/build.gradle.kts
+++ aes-library/build.gradle.kts
@@ -31,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -65,5 +65,5 @@ afterEvaluate {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
