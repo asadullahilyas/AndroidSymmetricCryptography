dd91a3eb2df1752c0877ab9db47de543b44bf37c
Asadullah Ilyas <asadullah.ilyas@packagex.io>
2024-01-11 19:02:37 +0500

    Android component removed from library

--- aes-library/build.gradle.kts
+++ aes-library/build.gradle.kts
@@ -1,8 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    kotlin("jvm") version "1.9.21"
     id("com.google.devtools.ksp").version("1.9.21-1.0.16")
     id("maven-publish")
 }
@@ -13,53 +12,19 @@ val groupName = "com.github.asadullahilyas"
 group = groupName
 version = artifactVersion
 
-android {
-    namespace = "com.asadullah.aes_library"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 29
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
-
 afterEvaluate {
     publishing {
         publications {
             create<MavenPublication>("maven") {
                 groupId = groupName
-                artifactId = "AndroidSymmetricEncryption"
+                artifactId = "KotlinSymmetricEncryption"
                 version = artifactVersion
 
                 from(components["java"])
             }
+        }
+        repositories {
+            mavenLocal()
         }
     }
 }
@@ -70,4 +35,9 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
